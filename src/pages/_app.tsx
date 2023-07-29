765a7b3b14ca7a603ad94a7871362515d0622a04
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-07-29 16:56:01 +0300

    initiating the project.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/src/styles/globals.css';
+import { EmotionCache } from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import type { AppProps as DefaultAppProps } from 'next/app';
+import createEmotionCache from '@/src/libs/createEmotionCache';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppProps extends DefaultAppProps {
+  emotionCache?: EmotionCache;
 }
+
+const theme = createTheme({});
+
+function App(props: AppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default App;
